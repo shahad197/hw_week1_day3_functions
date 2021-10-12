447ac5e04b7b1fe710db89c8dd0d436484baa356
shahad197 <shahad.yousef.jed.k2@tuwaiq.edu.sa>
2021-10-12 23:37:13 +0300

    Problem Solved :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,23 @@
-fun main(){   }
+fun main(){
+
+
+    fun addNum(num1: Any): Any {
+
+    }
+
+    fun result(){
+        println(addNum(num1:10,num2:12))
+
+
+    }
+
+    fun addNum(num1:Int , num2:Int) :Int{
+        return num1 + num2
+    }
+
+}
+
+fun fulName(firstName:string,midName:string,lastName:string):string{
+    return ("$firstName $midName $lastName")
+    
+}
